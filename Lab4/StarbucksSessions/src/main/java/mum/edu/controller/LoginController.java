73b5f9b52497936ae40e1566e5bce6f6193b6352
filiboy03 <filiboy03@gmail.com>
2017-11-03 22:06:09 -0500

    logincontroller

--- Lab4/StarbucksSessions/src/main/java/mum/edu/controller/LoginController.java
+++ Lab4/StarbucksSessions/src/main/java/mum/edu/controller/LoginController.java
@@ -3,11 +3,11 @@ package mum.edu.controller;
 import mum.edu.domain.User;
 import mum.edu.service.UserService;
 
-import javax.servlet.http.HttpSession;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -51,7 +51,7 @@ public class LoginController   {
 		redirect.addFlashAttribute("fillisession",user);
 		
 
-		//return "LoginSuccessful";
+	
 		return "redirect:advice";
 	}
 	  @RequestMapping (value={"/logout"},method = RequestMethod.GET)
